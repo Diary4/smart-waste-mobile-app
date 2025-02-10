a4a98adbfaf1070c94e0fd75edaf1d411645ddfb
Diary Salah <diarysalah36@gmail.com>
2025-02-10 16:43:20 +0300

    Updating File Structure

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,1 +1,128 @@
+import 'package:flutter/material.dart';
+import '../components/appbar.dart'; // Ensure this import is correct
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(), // Ensure AppBarWidget is defined in appbar.dart
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xFF77B254),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Hello , User",
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "Welcome to Smart Waste",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Text('Waste Type'),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10, left: 10),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color(0xFF5B913B),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF77B254),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "First Text",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF77B254),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Second Text",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF77B254),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Third Text",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF77B254),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Fourth Text", // Fixed typo here
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Color(0xFF77B254),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(
+                        "Fifth Text",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'components/appbar.dart';
+import 'home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,112 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Smart Waste',
-      home: Scaffold(
-        appBar: AppBarWidget(),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF77B254),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(children: [
-                    Text(
-                      "Hello , User",
-                      style: TextStyle(
-                          fontStyle: FontStyle.italic, color: Colors.white),
-                    ),
-                    Text(
-                      "Welcome to Smart Waste",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ]),
-                ),
-                Text('Waste Type'),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: EdgeInsets.only(right: 10, left: 10),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Color(0xFF5B913B),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Color(0xFF77B254),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Text(
-                          "First Text",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Color(0xFF77B254),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Text(
-                          "Second Text",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF77B254),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(
-                          "Third Text",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF77B254),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(
-                          "Foruth Text",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false, title: 'Smart Waste', home: Home());
   }
 }
